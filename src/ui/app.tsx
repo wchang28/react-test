5620f227e4929128b0e1458ae5535c750595722a
wchang28 <wchang28@hotmail.com>
2020-02-02 20:50:40 -0500

    make adding test simpler

--- src/ui/app.tsx
+++ src/ui/app.tsx
@@ -1,44 +1,54 @@
 import * as React from 'react';
 import {Test as TestNameEntry} from "./test-name-entry";
-import {Test as TestNameDialog} from "./test-dialog";
+import {Test as TestDialog} from "./test-dialog";
 
-export type TestComponentConstructor = new (props?: any) => any;
+type TestComponentConstructor = new (props?: any) => any;
+
+interface TestItem {
+	id: string;
+	name: string;
+	componentClass: TestComponentConstructor;
+}
+
+const testConfig: TestItem[] = [
+	{id: "name-entry", name: "Name Entry", componentClass: TestNameEntry}
+	,{id: "dialog", name: "Dialog", componentClass: TestDialog}
+	// TODO: add more test cases here
+];
 
 interface State {
 	testSelected?: string;
 }
 
 export class App extends React.Component<any, State> {
+	private tests: {[id: string]: TestItem};
 	constructor(props) {
 		super(props);
 		this.state = {
 			testSelected: ""
 		};
+		this.tests = {};
+		testConfig.forEach((item) => {
+			this.tests[item.id] = item;
+		})
 	}
 	onTestSelectChange(event: React.ChangeEvent<HTMLSelectElement>) {
 		this.setState({testSelected: event.target.value});
 	}
 	render() {
-		let testComponentClass: TestComponentConstructor = null;
-		switch(this.state.testSelected) {
-			case "name-entry":
-				testComponentClass = TestNameEntry;
-				break;
-			case "dialog":
-				testComponentClass = TestNameDialog;
-				break;
-		}
-		let testElement = (testComponentClass ? React.createElement(testComponentClass): null);
-		let testDiv = (testElement ? (<div className="w3-container w3-card-4 w3-border w3-margin-top">{testElement}</div>) : null);
+		const testComponentClass: TestComponentConstructor = (this.tests[this.state.testSelected] ? this.tests[this.state.testSelected].componentClass : null);
+		const testElement = (testComponentClass ? React.createElement(testComponentClass): null);
+		const testDiv = (testElement ? (<div className="w3-container w3-card-4 w3-border w3-margin-top">{testElement}</div>) : null);
+		const testSelectOptions = [(<option value="" disabled selected>Choose your test</option>)].concat(testConfig.map((item) => {
+			return (<option value={item.id}>{item.name}</option>);
+		}));
 		return (
 			<div className="w3-row-padding">
 				<div className="w3-third">
 					<div>
 						<label>Test Selection:</label>
 						<select className="w3-select w3-border" value={this.state.testSelected} onChange={this.onTestSelectChange.bind(this)}>
-							<option value="" disabled selected>Choose your option</option>
-							<option value="name-entry">Name Entry</option>
-							<option value="dialog">Dialog</option>
+							{testSelectOptions}
 						</select>
 					</div>
 					{testDiv}
