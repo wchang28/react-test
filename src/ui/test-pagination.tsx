2251e0973a8f5a8b4b5e33071b17684780e847b1
wchang28 <wchang28@hotmail.com>
2020-04-07 22:12:20 -0400

    added test

--- src/ui/test-pagination.tsx
+++ src/ui/test-pagination.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react';
 import {Pagination} from "./pagination";
+import * as pg from "./pagination";
+
+const allModes: pg.Mode[] = ["both","top","bottom"];
+const allFontSizes: pg.FontSize[] = ["tiny","small","medium","large","xlarge","xxlarge","xxxlarge","jumbo"];
 
 interface State {
     totalPages?: number;
     pageIndex?: number;
+    mode?: pg.Mode;
+    fontSize?: pg.FontSize;
     data?: any[];
 }
 
@@ -13,6 +19,8 @@ export class Test extends React.Component<any, State> {
 		this.state = {
             totalPages: 53
             ,pageIndex: 0
+            ,mode: "both"
+            ,fontSize: "small"
             ,data: []
 		};
     }
@@ -21,18 +29,42 @@ export class Test extends React.Component<any, State> {
             this.setState({pageIndex});
         }).bind(this);
     }
+    getOnModeChangeHandler() {
+        return ((event: React.ChangeEvent<HTMLSelectElement>) => {
+            this.setState({mode: event.target.value as pg.Mode});
+        }).bind(this);
+    }
+    getOnFontSizeChangeHandler() {
+        return ((event: React.ChangeEvent<HTMLSelectElement>) => {
+            this.setState({fontSize: event.target.value as pg.FontSize});
+        }).bind(this);
+    }
     render() {
+        const modeOptions = allModes.map((mode, index) => {
+            return (<option key={index} value={mode}>{mode}</option>);
+        });
+        const fontSizeOptions = allFontSizes.map((fontSize, index) => {
+            return (<option key={index} value={fontSize}>{fontSize}</option>);
+        });
         return (
             <div>
                 <div className="w3-container">
                     <p><b>Pagination Test</b></p>
-                    <div className="w3-container">
-                        <Pagination mode="both" fontSize="small" viewLength={10} totalPages={this.state.totalPages} pageIndex={this.state.pageIndex} onPageChange={this.getOnPageChangeHandler()}>
-                            <div className="w3-container">
-                                Showing content for page {this.state.pageIndex+1}
-                            </div>
-                        </Pagination>
-                    </div>
+                    <form className="w3-container w3-border w3-round w3-padding-16 w3-margin-bottom w3-small" style={{width:"33%"}}>
+                        <label>Mode</label>
+                        <select className="w3-select w3-border w3-round" name="mode" value={this.state.mode} onChange={this.getOnModeChangeHandler()}>
+                            {modeOptions}
+                        </select>
+                        <label>Font Size</label>
+                        <select className="w3-select w3-border w3-round" name="fontSize" value={this.state.fontSize} onChange={this.getOnFontSizeChangeHandler()}>
+                            {fontSizeOptions}
+                        </select>
+                    </form>
+                    <Pagination mode={this.state.mode} fontSize={this.state.fontSize} viewLength={10} totalPages={this.state.totalPages} pageIndex={this.state.pageIndex} onPageChange={this.getOnPageChangeHandler()}>
+                        <div className="w3-container">
+                            Showing content for page {this.state.pageIndex+1}
+                        </div>
+                    </Pagination>
                 </div>
             </div>
         );
