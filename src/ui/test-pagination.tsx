6f34f0db68ef56d2687918ebfdaf56cedb868f1e
wchang28 <wchang28@hotmail.com>
2020-04-08 02:09:26 -0400

    test color

--- src/ui/test-pagination.tsx
+++ src/ui/test-pagination.tsx
@@ -4,12 +4,45 @@ import * as pg from "./pagination";
 
 const allModes: pg.Mode[] = ["both","top","bottom"];
 const allFontSizes: pg.FontSize[] = ["tiny","small","medium","large","xlarge","xxlarge","xxxlarge","jumbo"];
+const allColors: pg.Color[] = [
+    "amber"
+    ,"aqua"
+    ,"blue"
+    ,"light-blue"
+    ,"brown"
+    ,"cyan"
+    ,"blue-grey"
+    ,"green"
+    ,"light-green"
+    ,"indigo"
+    ,"khaki"
+    ,"lime"
+    ,"orange"
+    ,"deep-orange"
+    ,"pink"
+    ,"purple"
+    ,"deep-purple"
+    ,"red"
+    ,"sand"
+    ,"teal"
+    ,"yellow"
+    ,"white"
+    ,"black"
+    ,"grey"
+    ,"light-grey"
+    ,"dark-grey"
+    ,"pale-red"
+    ,"pale-green"
+    ,"pale-yellow"
+    ,"pale-blue"
+];
 
 interface State {
     totalPages?: number;
     pageIndex?: number;
     mode?: pg.Mode;
     fontSize?: pg.FontSize;
+    selectedColor?: pg.Color;
     data?: any[];
 }
 
@@ -21,6 +54,7 @@ export class Test extends React.Component<any, State> {
             ,pageIndex: 0
             ,mode: "both"
             ,fontSize: "small"
+            ,selectedColor: "green"
             ,data: []
 		};
     }
@@ -39,12 +73,20 @@ export class Test extends React.Component<any, State> {
             this.setState({fontSize: event.target.value as pg.FontSize});
         }).bind(this);
     }
+    getOnSelectedColorChangeHandler() {
+        return ((event: React.ChangeEvent<HTMLSelectElement>) => {
+            this.setState({selectedColor: event.target.value as pg.Color});
+        }).bind(this);
+    }
     render() {
         const modeOptions = allModes.map((mode, index) => {
             return (<option key={index} value={mode}>{mode}</option>);
         });
         const fontSizeOptions = allFontSizes.map((fontSize, index) => {
             return (<option key={index} value={fontSize}>{fontSize}</option>);
+        });
+        const colorOptions = allColors.map((color, index) => {
+            return (<option key={index} value={color}>{color}</option>);
         });
         return (
             <div>
@@ -58,8 +100,12 @@ export class Test extends React.Component<any, State> {
                     <select className="w3-select w3-border w3-round" name="fontSize" value={this.state.fontSize} onChange={this.getOnFontSizeChangeHandler()}>
                         {fontSizeOptions}
                     </select>
+                    <label>Selected Color</label>
+                    <select className="w3-select w3-border w3-round" name="selectedColor" value={this.state.selectedColor} onChange={this.getOnSelectedColorChangeHandler()}>
+                        {colorOptions}
+                    </select>
                 </form>
-                <Pagination mode={this.state.mode} fontSize={this.state.fontSize} selectedColor="red" viewLength={10} totalPages={this.state.totalPages} pageIndex={this.state.pageIndex} onPageChange={this.getOnPageChangeHandler()}>
+                <Pagination mode={this.state.mode} fontSize={this.state.fontSize} selectedColor={this.state.selectedColor} viewLength={10} totalPages={this.state.totalPages} pageIndex={this.state.pageIndex} onPageChange={this.getOnPageChangeHandler()}>
                     <div className="w3-container w3-border" style={{marginTop: "8px", marginBottom: "8px"}}>
                         <p>Showing content for page {this.state.pageIndex+1}</p>
                     </div>
