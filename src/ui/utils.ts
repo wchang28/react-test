96b2fe5a56acf54b0c9dfd0a94d7c3b6eb33f8a9
wchang28 <wchang28@hotmail.com>
2020-06-09 20:22:58 -0400

    added css uglify

--- src/ui/utils.ts
+++ src/ui/utils.ts
@@ -1,6 +1,11 @@
-export function injectCSS(css: string) {
+import * as uglifycss from "uglify-css";
+
+export function injectCSS(css: string, uglify: boolean = false) {
     const style = document.createElement('style');
     style.setAttribute("type", "text/css");
+    if (uglify) {
+        css = uglifycss.processString(css);
+    }
     style.appendChild(document.createTextNode(css));
     document.getElementsByTagName("head")[0].appendChild(style); 
 }
