f4a57c70872047e3838e4afa78599522d992d623
wchang28 <wchang28@hotmail.com>
2020-06-12 15:09:26 -0400

    update

--- src/ui/test-functional-hooks.tsx
+++ src/ui/test-functional-hooks.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import {useState, useEffect} from 'react';
 
 export default () => {
+    console.log(`calling render()...`);
     const [isActive] = useState<boolean>(true);
     const [currentTime, setCurrentTime] = useState<number>(new Date().getTime());
     useEffect(() => {
