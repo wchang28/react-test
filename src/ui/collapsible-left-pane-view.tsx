2a774cf2fe9597e7d2aef4f90b9aa41d54795a30
wchang28 <wchang28@hotmail.com>
2020-06-12 19:43:47 -0400

    update

--- src/ui/collapsible-left-pane-view.tsx
+++ src/ui/collapsible-left-pane-view.tsx
@@ -2,6 +2,7 @@
 import * as React from "react";
 
 const DEFAULT_LEDT_PANEL_WIDTH_PX = 300;
+const TOGGLE_AREA_DIM_PX = 24;
 
 export interface Props {
     collapsed?: boolean;
@@ -9,14 +10,85 @@ export interface Props {
     onCollapseChanged: (collapsed: boolean) => void;
 }
 
+const leftPaneContent = (
+<div className="left-content">
+    Hello,---------World!<br/>
+    Hello,---------World!<br/>
+    Hello,---------World!<br/>
+    Hello,---------World!<br/>
+    Hello,---------World!<br/>
+    Hello,---------World!<br/>
+    Hello,---------World!<br/>
+    Hello,---------World!<br/>
+</div>
+);
+
+const rightPaneContent = (
+<div className="content">
+    Hello, World!<br/>
+    Hello, World!<br/>
+    Hello, World!<br/>
+    Hello, World!<br/>
+    Hello, World!<br/>
+</div>
+);
+
+const getToggleArea = (collapsed: boolean, onToggleClicked: () => void) => {
+    const icon = (collapsed ? "arrow-circle-o-right" : "arrow-circle-o-left");
+    return (
+        <div className="clpv-toggle-area" style={{float:"right", height: `${TOGGLE_AREA_DIM_PX}px`, width: `${TOGGLE_AREA_DIM_PX}px`, position: "relative"}}>
+            <i className={`clpv-toggle-button fa fa-${icon}`} style={{fontSize:"1.25em", margin:0, position:"absolute", top:"50%", left:"50%", transform:"translate(-50%, -50%)", cursor:"pointer"}} onClick={onToggleClicked}></i>
+        </div>
+    );
+};
+
+const getLeftContainer = (collapsed: boolean, content: JSX.Element) => {
+    const float = (collapsed ? "left" : undefined);
+    const width = (collapsed ? "0" : undefined);
+    return (
+        <div className="clpv-left-content-container" style={{float, width, display: "block", overflow:"auto", whiteSpace: "nowrap"}}>
+            {content}
+        </div>
+    );
+}
+
+const getLeftView = (collapsed: boolean, onToggleClicked: () => void, content: JSX.Element) => {
+    if (collapsed) {
+        return (
+            <div className="clpv-view-collpase w3-bar">
+                {getLeftContainer(collapsed, content)}
+                {getToggleArea(collapsed, onToggleClicked)}
+            </div>
+        );
+    } else {
+        return (
+            <div className="clpv-view-no-collpase">
+                <div className="clpv-left-pane-top-bar w3-bar">
+                    {getToggleArea(collapsed, onToggleClicked)}
+                </div>
+                {getLeftContainer(collapsed, content)}
+            </div>
+        );
+    }
+}
+
 export default (props: Props) => {
     const collapsed = (typeof props.collapsed === "boolean" ? props.collapsed : false);
     const leftPaneWidthPx = (typeof props.leftPaneWidthPx === "number" && props.leftPaneWidthPx >= 0 ? props.leftPaneWidthPx : DEFAULT_LEDT_PANEL_WIDTH_PX);
-    const debugObj = {collapsed, leftPaneWidthPx};
-    const debugButton = <button className="w3-bar-item w3-botton w3-border w3-round" onClick={() => {props.onCollapseChanged(!collapsed);}}>Toggle Me {`(${JSON.stringify(debugObj)})`}</button>
+    const onToggleClicked = () => {props.onCollapseChanged(!collapsed);};
+    const leftWidth = (collapsed ? TOGGLE_AREA_DIM_PX : leftPaneWidthPx);
+    //const debugObj = {collapsed, leftPaneWidthPx};
+    //const debugButton = <button className="w3-bar-item w3-botton w3-border w3-round" onClick={onToggleClicked}>Toggle Me {`(${JSON.stringify(debugObj)})`}</button>
     return (
-        <div className="w3-bar">
-            {debugButton}
+        <div className="clpv-main w3-bar">
+            <div className="clpv-left" style={{float:"left", width:`${leftWidth}px`}}>
+                {getLeftView(collapsed, onToggleClicked, leftPaneContent)}
+            </div>
+            <div className="clpv-right" style={{marginLeft:`${leftWidth}px`}}>
+                <div className="clpv-right-content-container">
+                    {rightPaneContent}
+                </div>
+            </div>
         </div>
     )
 }
