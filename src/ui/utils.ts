1b4226395c04a4795fde4910c60e2f9cb54ae58f
wchang28 <wchang28@hotmail.com>
2020-06-13 12:10:24 -0400

    added uuid

--- src/ui/utils.ts
+++ src/ui/utils.ts
@@ -1,5 +1,15 @@
 import * as uglifycss from "uglify-css";
 
+export function uuid() {
+    let dt = new Date().getTime();
+    const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+        const r = (dt + Math.random()*16)%16 | 0;
+        dt = Math.floor(dt/16);
+        return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+}
+
 export function injectCSS(css: string, uglify: boolean = false) {
     const style = document.createElement('style');
     style.setAttribute("type", "text/css");
