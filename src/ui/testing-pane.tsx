c2f4b50971af5ea08827877fd30edec3af815819
wchang28 <wchang28@hotmail.com>
2020-06-13 14:59:27 -0400

    update

--- src/ui/testing-pane.tsx
+++ src/ui/testing-pane.tsx
@@ -7,12 +7,12 @@ export default (props: ReactProps) => {
     const displayPane = props.children[1];
     return (
         <div>
-        <div className="w3-container w3-border" style={{padding: "0 8px", marginTop: "8px"}}>
-            {cofigurationPane}
+            <div className="w3-container w3-border" style={{padding: "0 8px", marginTop: "8px"}}>
+                {cofigurationPane}
+            </div>
+            <div className="w3-container w3-border" style={{paddingTop: "8px", paddingBottom: "8px", marginTop: "8px"}}>
+                {displayPane}
+            </div>
         </div>
-        <div className="w3-container w3-border" style={{paddingTop: "8px", paddingBottom: "8px", marginTop: "8px"}}>
-            {displayPane}
-        </div>
-    </div>
     )
 }
