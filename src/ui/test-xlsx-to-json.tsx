8d7dbe3635459aab78074795aac2222344b35824
wchang28 <wchang28@hotmail.com>
2020-06-14 22:03:40 -0400

    update

--- src/ui/test-xlsx-to-json.tsx
+++ src/ui/test-xlsx-to-json.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import {TestingPane} from "./test-common";
+import FileDragDropSelect from "./file-drag-drop-select";
+import XLSX2JSON from "./browser-xlsx-to-json";
 
 export default () => {
+    const onFileSelect = async (files: File[]) => {
+        if (files.length === 1) {
+            const o = await XLSX2JSON(files[0]);
+            console.log(JSON.stringify(o, null, 2));
+        }
+    }
     return (
         <TestingPane>
-            <div>
-                XLSX
+            <div style={{width:"50%"}}>
+                <FileDragDropSelect
+                    accept=".xlsx"
+                    multiple={false}
+                    onFileSelect={onFileSelect}
+                />
             </div>
         </TestingPane>
     );
