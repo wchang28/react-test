fc6f6eb6c990a5e64ceeec628794430385b22e9c
wchang28 <wchang28@hotmail.com>
2020-06-16 13:36:40 -0400

    update

--- src/ui/property-grid-editor.tsx
+++ src/ui/property-grid-editor.tsx
@@ -10,13 +10,19 @@ const defaultColumnProperties = {
     resizable: true
 };
 
+export interface PatchItem {
+    index: number;
+    patch: any;
+};
+
 export interface Props {
     objs: any[];
     propertyDefs: PropertyDef[];
     onChange: (objs: any[]) => void;
+    onPatch?: (patches: PatchItem[]) => void;
     customEditors?: PropertyCustomEditor[];
     customFormatters?: PropertyCustomFormatter[];
-    propertyNameHeader?: string;
+    heading?: string;
     objColumnHeader?: (obj: any) => string;
     minHeight?: number;
 }
@@ -24,7 +30,7 @@ export interface Props {
 type ReactProps<P = unknown> = Readonly<P> & Readonly<{ children?: ReactNode }>;
 
 export default (props: ReactProps<Props>) => {
-    const {objs, propertyDefs, onChange, customEditors, customFormatters, propertyNameHeader, objColumnHeader, minHeight} = props;
+    const {objs, propertyDefs, onChange, onPatch, customEditors, customFormatters, heading, objColumnHeader, minHeight} = props;
     const editor = <PropertyBasedEditor customEditors={customEditors}/>
     const formatter = <PropertyBasedFormatter customFormatters={customFormatters}/>
     // pivot objs to rows
@@ -45,13 +51,13 @@ export default (props: ReactProps<Props>) => {
         return {key: `object_${index}`, name: (objColumnHeader ? objColumnHeader(obj) : "Value"), editor, formatter} as any;
     });
     const columns: any[] = [
-        { key: "propName", name: propertyNameHeader ? propertyNameHeader : "Property"}
+        { key: "propName", name: heading ? heading : ""}
     ].concat(objColumns)
     .map(c => ({...c, ...defaultColumnProperties}));
     ////////////////////////////////////////////////////////////////////////////////////////////////////
     const onGridRowsUpdated = (update: { fromRow: number, toRow: number, updated: any }) => {
         const {fromRow, toRow, updated} = update;
-        console.log(`onGridRowsUpdated(): fromRow=${fromRow}, toRow=${toRow}, updated=${JSON.stringify(updated)}`);
+        //console.log(`onGridRowsUpdated(): fromRow=${fromRow}, toRow=${toRow}, updated=${JSON.stringify(updated)}`);
         const rowsCopy = rows.slice();
         for (let i = fromRow; i <= toRow; i++) {
             rowsCopy[i] = { ...rowsCopy[i], ...updated };
@@ -65,7 +71,7 @@ export default (props: ReactProps<Props>) => {
             const oldObj = objs[i];
             const o = {};
             for (const row of rowsCopy) {   // for each row/property
-                const propId = row["propId"];
+                const {propId} = (row as PropertyDef);
                 o[propId] = row[valueKey];
             }
             const newObj = {...oldObj, ...o};
@@ -73,6 +79,24 @@ export default (props: ReactProps<Props>) => {
         }
         ////////////////////////////////////////////////////////////////////////////////////////
         onChange(newObjs);
+        // notifying object being patched
+        ////////////////////////////////////////////////////////////////////////////////////////
+        const patches: PatchItem[] = [];
+        for (const key of Object.keys(updated)) {   // for each object
+            const x = key.indexOf("_");
+            if (x !== -1) {
+                const index = parseInt(key.substr(x+1));
+                const value = updated[key];
+                const patch = {};
+                for (let rowindex=fromRow; rowindex <= toRow; rowindex++) { // for each property
+                    const {propId} = rows[rowindex] as PropertyDef;
+                    patch[propId] = value;
+                }
+                patches.push({index, patch});
+            }
+        }
+        onPatch(patches);
+        ////////////////////////////////////////////////////////////////////////////////////////
     };
     return (
         <ReactDataGrid
--- src/ui/test-objs-property-editor.tsx
+++ src/ui/test-objs-property-editor.tsx
@@ -1,37 +1,10 @@
 import React, {useState} from "react";
-import {TestingPane} from "./test-common";
 import {Editors} from "react-data-grid-addons";
-import {PropertyDef, PropertyCustomEditor, Editors as EditorsExt, Formatters as FormattersExt} from "react-data-grid-addons-extension";
-import JSONTree from 'react-json-tree';
-const ReactDataGrid = require("react-data-grid");
+import PropertyGridEditor, {PropertyDef, PropertyCustomEditor, PatchItem} from "./property-grid-editor";
+import {Editors as EditorsExt} from "react-data-grid-addons-extension";
+import {TestingPane, ConfigurationPane, FontSize, getFontSizeSelector, FontSizeColorTestingWrapper, getNumberInput, getTextInput} from "./test-common";
 import {uuid, injectCSS} from "./utils";
-
-const {DropDownEditor} = Editors;
-const {NumericInputEditor, PropertyBasedEditor} = EditorsExt;
-const {PropertyBasedFormatter} = FormattersExt;
-
-const issueTypes: Editors.DropDown.OptionItem[] = [
-    { id: "bug", value: "Bug" },
-    { id: "epic", value: "Epic" },
-    { id: "story", value: "Story" }
-];
-const IssueTypeEditor = <DropDownEditor options={issueTypes} />;
-const completeEditor = <NumericInputEditor min={0} max={100} step={2}/>
-
-interface Item {
-    id: number;
-    title: string | null;
-    issueType?: "Bug" | "Epic" | "Story" | null;
-    complete?: number | null;
-    enabled?: boolean | null;
-    settleDate?: string | null;
-}
-
-const objs: Item[] = [
-    { id: 0, title: "Task 1", issueType: "Bug", complete: 20, enabled: true, settleDate: "2020-04-26" },
-    { id: 1, title: "Task 2", issueType: "Story", complete: 40 , enabled: false, settleDate: null },
-    { id: 2, title: "Task 3", issueType: "Epic", complete: 60, enabled: true, settleDate: "2020-06-02" }
-];
+import JSONTree from 'react-json-tree';
 
 const propertyDefs: PropertyDef[] = [
     {propId: "id", propName: "ID", propType: "string", propReadOnly: true}
@@ -42,38 +15,36 @@ const propertyDefs: PropertyDef[] = [
     ,{propId: "settleDate", propName: "Settle Date", propType: "date"}
 ];
 
+const {DropDownEditor} = Editors;
+const {NumericInputEditor} = EditorsExt;
+
+const issueTypes: Editors.DropDown.OptionItem[] = [
+    { id: "bug", value: "Bug" },
+    { id: "epic", value: "Epic" },
+    { id: "story", value: "Story" }
+];
+const IssueTypeEditor = <DropDownEditor options={issueTypes} />;
+const completeEditor = <NumericInputEditor min={0} max={100} step={2}/>
+
 const customEditors: PropertyCustomEditor[] = [
     {propId: "issueType", editor: IssueTypeEditor}
     ,{propId: "complete", editor: completeEditor}
 ];
 
-const editor = <PropertyBasedEditor customEditors={customEditors}/>
-const formatter = <PropertyBasedFormatter/>
+interface Row {
+    id: number;
+    title: string | null;
+    issueType?: "Bug" | "Epic" | "Story" | null;
+    complete?: number | null;
+    enabled?: boolean | null;
+    settleDate?: string | null;
+}
 
-const rows = propertyDefs.map(({propId, propName, propType, propReadOnly}) => {  // for each property
-    if (!propName) propName = propId;
-    const propAtrib = {propId, propName, propType, propReadOnly};
-    const o: any = {};
-    objs.forEach((obj, index) => {   // for each object
-        const column = `object_${index}`;
-        const value = obj[propId];
-        o[column] = value;
-    });
-    return {...propAtrib, ...o};
-});
-
-const defaultColumnProperties = {
-    resizable: true
-};
-
-const objColumns: any[] = objs.map((obj, index) => {  // for each object
-    return {key: `object_${index}`, name: obj.id, editor, formatter} as any;
-})
-
-const columns: any[] = [
-    { key: "propName", name: "Property Name"}
-].concat(objColumns)
-.map(c => ({...c, ...defaultColumnProperties}));
+const rows: Row[] = [
+    { id: 0, title: "Task 1", issueType: "Bug", complete: 20, enabled: true, settleDate: "2020-04-26" },
+    { id: 1, title: "Task 2", issueType: "Story", complete: 40 , enabled: false, settleDate: null },
+    { id: 2, title: "Task 3", issueType: "Epic", complete: 60, enabled: true, settleDate: "2020-06-02" }
+];
 
 const this_class = `test-objs-property-editor-${uuid()}`;
 
@@ -93,28 +64,41 @@ injectCSS(`
 `);
 
 export default () => {
-    const [data, setData] = useState<any[]>(rows);
-    const onGridRowsUpdated = (update: { fromRow: number, toRow: number, updated: any }) => {
-        const {fromRow, toRow, updated} = update;
-        const rows = data.slice();
-        for (let i = fromRow; i <= toRow; i++) {
-            rows[i] = { ...rows[i], ...updated };
-        }
-        setData(rows);
-    }
+    const [objs, setObjs] = useState<any[]>(rows);
+    const [patches, setPatches] = useState<PatchItem[]>([]);
+    const [heading, setHeading] = useState("Property");
+    const [minHeight, setMinHeight] = useState(350);
+    const [fontSize, setFontSize] = useState<FontSize>("medium");
     return (
         <TestingPane testingClassName={this_class}>
-            <div className="w3-container w3-tiny w3-margin-top">
-                <div style={{width:"75%"}}>
-                    <ReactDataGrid
-                        columns={columns}
-                        rowGetter={(i: number) => data[i]}
-                        rowsCount={data.length}
-                        minHeight={650}
-                        onGridRowsUpdated={onGridRowsUpdated}
-                        enableCellSelect={true}
-                    />
-                </div>
+            <ConfigurationPane>
+                {getTextInput("heading", heading, setHeading)}
+                {getNumberInput("minHeight", minHeight, setMinHeight)}
+                {getFontSizeSelector(fontSize, setFontSize)}
+            </ConfigurationPane>
+            <div className="w3-container">
+                <FontSizeColorTestingWrapper fontSize={fontSize}>
+                    <div className="w3-margin-top" style={{width:"75%"}}>
+                        <PropertyGridEditor
+                            objs={objs}
+                            propertyDefs={propertyDefs}
+                            customEditors={customEditors}
+                            onChange={setObjs}
+                            onPatch={setPatches}
+                            minHeight={minHeight}
+                            heading={heading}
+                            objColumnHeader={(obj: Row) => obj.id.toString()}
+                        />
+                    </div>
+                    <div className="w3-margin-top" style={{width:"50%"}}>
+                        <div>objs:</div>
+                        <JSONTree data={objs} />
+                    </div>
+                    <div className="w3-margin-top" style={{width:"50%"}}>
+                        <div>onPatch(patches):</div>
+                        <JSONTree data={patches} />
+                    </div>
+                </FontSizeColorTestingWrapper>
             </div>
         </TestingPane>
     );
