5cf4cf4a744a0c970cac50df53d264e372cec931
wchang28 <wchang28@hotmail.com>
2020-07-20 16:00:16 -0400

    update

--- src/ui/splitter-view.tsx
+++ src/ui/splitter-view.tsx
@@ -1,170 +1,111 @@
 import * as React from 'react';
+import {ReactNode, useRef} from "react";
 
 export type Direction = "vertical" | "horizontal";
 
-const DEFAULT_DIRECTION: Direction = "vertical";
-const DEFAULT_FIRST_PANE_SIZE: number = 30;
-const DEFAULT_SPLITTER_SIZE_PX: number = 3;
-
 export interface Props {
-    direction: Direction;
-    splitterSizePx?: number;
-    defaultFirstPaneSizePct?: number;
-}
-
-export interface State {
     direction?: Direction;
     splitterSizePx?: number;
-    firstPaneSize?: string; // include splitter
+    dividerPosition?: number;
+    onChange?: (dividerPosition: number) => void;
 }
 
-export class SplitterView extends React.Component<Props, State> {
-    private refMainContainer: React.RefObject<HTMLDivElement>;
-    private refFirstPane: React.RefObject<HTMLDivElement>;
-    private docMouseMoveListener: (event: MouseEvent) => void;
-    private docMouseUpListener: (event: MouseEvent) => void;
-    constructor(props) {
-        super(props);
-        this.refMainContainer = React.createRef<HTMLDivElement>();
-        this.refFirstPane = React.createRef<HTMLDivElement>();
-        this.docMouseMoveListener = this.DocumentMouseMoveListener;
-        this.docMouseUpListener = this.DocumentMouseUpListener;
-        this.state = {
-            direction: null
-            ,splitterSizePx: null
-            ,firstPaneSize: `${(this.props.defaultFirstPaneSizePct ? this.props.defaultFirstPaneSizePct : DEFAULT_FIRST_PANE_SIZE)}%`
-        };
+type ReactProps<P = unknown> = Readonly<P> & Readonly<{ children?: ReactNode }>;
+
+export default function SplitterView(props: ReactProps<Props>) {
+    const {direction, splitterSizePx, dividerPosition, onChange, children} = props;
+    const refMainContainer = useRef<HTMLDivElement>();
+    const refFirstPane = useRef<HTMLDivElement>();
+    const firstPaneSize = `${dividerPosition}%`;
+    // styleMainContainer
+    const styleMainContainer: React.CSSProperties = {position: "relative", width: "100%", height: "100%"};
+    // styleFirstPane
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    const styleFirstPane: React.CSSProperties = {position: "absolute", left: 0, top: 0};
+    if (direction === "vertical") {
+        styleFirstPane.bottom = 0;
+        styleFirstPane.width = firstPaneSize;
+    } else {    // horizontal
+        styleFirstPane.right = 0;
+        styleFirstPane.height = firstPaneSize;
     }
-    static getDerivedStateFromProps(nextProps: Props, prevState: State) {
-        const ret: State = {};
-        if (nextProps.direction !== prevState.direction) {
-            ret.direction = nextProps.direction;
-        }
-        if (nextProps.splitterSizePx != undefined && nextProps.splitterSizePx !== prevState.splitterSizePx) {
-            ret.splitterSizePx = nextProps.splitterSizePx;
-        }
-        return (JSON.stringify(ret) === '{}' ? null : ret);
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    // styleSecondPane
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    const styleSecondPane: React.CSSProperties = {position: "absolute", bottom: 0, right: 0};
+    if (direction === "vertical") {
+        styleSecondPane.top = 0;
+        styleSecondPane.left = firstPaneSize;
+    } else {    // horizontal
+        styleSecondPane.left = 0;
+        styleSecondPane.top = firstPaneSize;
     }
-    get DocumentMouseMoveListener() {
-        return ((event: MouseEvent) => {
-            const mainContainer = this.refMainContainer.current;
-            const mainContainerRect = mainContainer.getBoundingClientRect();
-            const firstPaneDiv = this.refFirstPane.current;
-            const rect = firstPaneDiv.getBoundingClientRect();
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    // styleFirstPaneInner
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    const styleFirstPaneInner: React.CSSProperties = {position:"absolute", left: 0, top: 0};
+    if (direction === "vertical") {
+        styleFirstPaneInner.bottom = 0;
+        styleFirstPaneInner.right = `${splitterSizePx}px`;
+    } else {    // horizontal
+        styleFirstPaneInner.right = 0;
+        styleFirstPaneInner.bottom = `${splitterSizePx}px`;
+    }
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    // styleDivider
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    const styleDivider: React.CSSProperties = {position: "absolute", bottom: 0, right: 0, cursor: `${direction === "vertical" ? "col" : "row"}-resize`};
+    if (direction === "vertical") {
+        styleDivider.top = 0;
+        styleDivider.width = `${splitterSizePx}px`;
+    } else {    // horizontal
+        styleDivider.left = 0;
+        styleDivider.height = `${splitterSizePx}px`;
+    }
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    const onDividerMouseDown = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        e.preventDefault();
+        const docMouseMoveListener = (event: MouseEvent) => {
+            const mainContainerRect = refMainContainer.current.getBoundingClientRect();
+            const rect = refFirstPane.current.getBoundingClientRect();
             const x = event.clientX;
             const y = event.clientY;
             const offsetX = x - rect.x;
             const offsetY = y - rect.y;
-            const offset = (this.Direction === "vertical" ? offsetX : offsetY);
-            let firstPaneSizePx = offset + Math.floor(this.SplitterSizePx/2);
-            const minFirstPaneSizePx = this.SplitterSizePx;
-            const maxFirstPaneSizePx = (this.Direction === "vertical" ? mainContainerRect.width : mainContainerRect.height);
+            const offset = (direction === "vertical" ? offsetX : offsetY);
+            let firstPaneSizePx = offset + Math.floor(splitterSizePx/2);
+            const minFirstPaneSizePx = splitterSizePx;
+            const maxFirstPaneSizePx = (direction === "vertical" ? mainContainerRect.width : mainContainerRect.height);
             firstPaneSizePx = Math.min(Math.max(firstPaneSizePx, minFirstPaneSizePx), maxFirstPaneSizePx);
-            const containerSizePx = (this.Direction === "vertical" ? mainContainerRect.width : mainContainerRect.height);
-            const percent = (containerSizePx ? firstPaneSizePx*100.0/containerSizePx : 0);
-            this.setState({firstPaneSize: `${percent}%`});
-        }).bind(this);
-    }
-    get DocumentMouseUpListener() {
-        return ((event: MouseEvent) => {
-            document.removeEventListener("mousemove", this.docMouseMoveListener);
-            document.removeEventListener("mouseup", this.docMouseUpListener);
-        }).bind(this);
-    }
-    get OnDragbarMouseDownHandler() {
-        return ((e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-            e.preventDefault();
-            document.addEventListener("mousemove", this.docMouseMoveListener);
-            document.addEventListener("mouseup", this.docMouseUpListener);
-        }).bind(this);
-    }
-    get Direction() {
-        return (this.state.direction ? this.state.direction : DEFAULT_DIRECTION);
-    }
-    get SplitterSizePx() {
-        return (typeof this.state.splitterSizePx === "number" && this.state.splitterSizePx > 0 ? this.state.splitterSizePx : DEFAULT_SPLITTER_SIZE_PX);
-    }
-    render() {
-        const firstPaneSize = this.state.firstPaneSize;
-        const splitterSizePx = this.SplitterSizePx;
-        const direction = this.Direction;
-        // styleFirstPane
-        /////////////////////////////////////////////////////////////////////////////////////
-        const styleFirstPane: React.CSSProperties = {
-            position: "absolute"
-            ,left: "0px"
-            ,top: "0px"
+            const containerSizePx = (direction === "vertical" ? mainContainerRect.width : mainContainerRect.height);
+            const dividerPosition = (containerSizePx ? firstPaneSizePx*100.0/containerSizePx : 0);
+            onChange(dividerPosition);
         };
-        if (direction === "vertical") {
-            styleFirstPane.bottom = "0px";
-            styleFirstPane.width = firstPaneSize;
-        } else {    // horizontal
-            styleFirstPane.right = "0px";
-            styleFirstPane.height = firstPaneSize;
-        }
-        /////////////////////////////////////////////////////////////////////////////////////
-        // styleSecondPane
-        /////////////////////////////////////////////////////////////////////////////////////
-        const styleSecondPane: React.CSSProperties = {
-            position: "absolute"
-            ,bottom: "0px"
-            ,right: "0px"
+        const docMouseUpListener = (event: MouseEvent) => {
+            event.preventDefault();
+            document.removeEventListener("mousemove", docMouseMoveListener);
+            document.removeEventListener("mouseup", docMouseUpListener);
         };
-        if (direction === "vertical") {
-            styleSecondPane.top = "0px";
-            styleSecondPane.left = firstPaneSize;
-        } else {    // horizontal
-            styleSecondPane.left = "0px";
-            styleSecondPane.top = firstPaneSize;
-        }
-        /////////////////////////////////////////////////////////////////////////////////////
-        // styleFirstPaneInner
-        /////////////////////////////////////////////////////////////////////////////////////
-        const styleFirstPaneInner: React.CSSProperties = {
-            position:"absolute"
-            ,left:"0px"
-            ,top:"0px"
-        };
-        if (direction === "vertical") {
-            styleFirstPaneInner.bottom = "0px";
-            styleFirstPaneInner.right = `${splitterSizePx}px`;
-        } else {    // horizontal
-            styleFirstPaneInner.right = "0px";
-            styleFirstPaneInner.bottom = `${splitterSizePx}px`;
-        }
-        /////////////////////////////////////////////////////////////////////////////////////
-        // styleDragbar
-        /////////////////////////////////////////////////////////////////////////////////////
-        const styleDragbar: React.CSSProperties = {
-            position: "absolute"
-            ,bottom: "0px"
-            ,right: "0px"
-            ,cursor: `${direction === "vertical" ? "col" : "row"}-resize`
-        };
-        if (direction === "vertical") {
-            styleDragbar.top = "0px";
-            styleDragbar.width = `${splitterSizePx}px`;
-        } else {    // horizontal
-            styleDragbar.left = "0px";
-            styleDragbar.height = `${splitterSizePx}px`;
-        }
-        /////////////////////////////////////////////////////////////////////////////////////
-        const firstPaneContent = this.props.children[0];
-        const secondPaneContent = this.props.children[1];
-        return (
-            <div ref={this.refMainContainer} style={{position: "relative", width: "100%", height:"100%"}}>
-                <div ref={this.refFirstPane} style={styleFirstPane}>
-                    <div style={{position: "relative", width: "100%", height:"100%"}}>
-                        <div style={styleFirstPaneInner}>
-                            {firstPaneContent}
-                        </div>
-                        <div style={styleDragbar} onMouseDown={this.OnDragbarMouseDownHandler}></div>
-                    </div>
-                </div>
-                <div style={styleSecondPane}>
-                    {secondPaneContent}
-                </div>                
-            </div>
-        );
-    }
+        document.addEventListener("mousemove", docMouseMoveListener);
+        document.addEventListener("mouseup", docMouseUpListener);
+    };
+    const firstPaneContent = children[0];
+    const secondPaneContent = children[1];
+    const firstPaneInner = <div style={styleFirstPaneInner}>{firstPaneContent}</div>;
+    const divider = <div style={styleDivider} onMouseDown={onDividerMouseDown}></div>;
+    const firstPane = <div ref={refFirstPane} style={styleFirstPane}>{firstPaneInner}{divider}</div>;
+    const secondPane = <div style={styleSecondPane}>{secondPaneContent}</div>;
+    return (
+        <div ref={refMainContainer} style={styleMainContainer}>
+            {firstPane}
+            {secondPane}
+        </div>
+    );
 }
+
+SplitterView.defaultProps = {
+    direction: "vertical"
+    ,splitterSizePx: 5
+    ,dividerPosition: 20
+    ,onChange: () =>{}
+} as Props;
--- src/ui/test-splitter-view.tsx
+++ src/ui/test-splitter-view.tsx
@@ -1,10 +1,12 @@
 import React, {useState} from 'react';
-import {SplitterView} from "./splitter-view";
 import ScrollView from "./scroll-view";
 import NoOverflowContainer from "./no-overflow-container";
 import {TestingPane, ConfigurationPane, getFontSizeSelector, FontSize, getNumberInput, FontSizeColorTestingWrapper, getTestTable} from "./test-common";
+import SplitterView from "./splitter-view";
 
 export default () => {
+    const [dividerPositionVertical, setDividerPositionVertical] = useState(20);
+    const [dividerPositionHorizontal, setDividerPositionHorizontal] = useState(75);
     const [testAreaHeightPx, setTestAreaHeightPx] = useState(550);
     const [splitterWidthPx, setSplitterWidthPx] = useState(5);
     const [fontSize, setFontSize] = useState<FontSize>("small");
@@ -17,13 +19,13 @@ export default () => {
             </ConfigurationPane>
             <FontSizeColorTestingWrapper fontSize={fontSize}>
                 <div style={{height: `${testAreaHeightPx}px`}}>
-                    <SplitterView direction="vertical" splitterSizePx={splitterWidthPx} defaultFirstPaneSizePct={20}>
+                    <SplitterView direction="vertical" splitterSizePx={splitterWidthPx} dividerPosition={dividerPositionVertical} onChange={setDividerPositionVertical}>
                         <ScrollView>
                             <div className="w3-khaki" style={{width:"300px", height:"2000px"}}>
                                 First Pane
                             </div>
                         </ScrollView>
-                        <SplitterView direction="horizontal" defaultFirstPaneSizePct={75} splitterSizePx={splitterWidthPx}>
+                        <SplitterView direction="horizontal" splitterSizePx={splitterWidthPx} dividerPosition={dividerPositionHorizontal} onChange={setDividerPositionHorizontal}>
                             <ScrollView>
                                 <div className="w3-pale-green" style={{height:"1500px", width:"1300px"}}>
                                     Second Pane
